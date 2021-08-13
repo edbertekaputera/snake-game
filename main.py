c4230484b8bdb6e4bf8a4a232dcf6028c3fb5011
Edbert Ekaputera <edbertekaputera@gmail.com>
2021-08-13 14:21:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import time
-import random
+from random import randrange
 
 
 #Constants (UPPERCASE NAMES)
@@ -17,6 +17,7 @@ ORANGE = (191, 93, 57)
 BLACK = (0,0,0)
 BLUE = (29, 140, 204)
 RED = (255,0,0)
+
 
 #2D Gameboard Screen Initiation
 pygame.init()
@@ -44,40 +45,89 @@ def textbox(font, txt, colour,pos):
 	text = font.render(txt, True, colour)
 	screen.blit(text, pos)
 
-#SNAKE FUNCTION
-def snake(snake_segments):
-	for segment in snake_segments:
-		pygame.draw.rect(screen, BLUE, [segment[0], segment[1], 20, 20])
-	if direction == "up":
-		pygame.draw.circle(screen, BLACK, (snake_segments[-1][0]+5,snake_segments[-1][1]+5), 2)
-		pygame.draw.circle(screen, BLACK, (snake_segments[-1][0]+15,snake_segments[-1][1]+5), 2)
-	elif direction == "down":
-		pygame.draw.circle(screen, BLACK, (snake_segments[-1][0]+5,snake_segments[-1][1]+15), 2)
-		pygame.draw.circle(screen, BLACK, (snake_segments[-1][0]+15,snake_segments[-1][1]+15), 2)
-	elif direction == "left":
-		pygame.draw.circle(screen, BLACK, (snake_segments[-1][0]+5,snake_segments[-1][1]+5), 2)
-		pygame.draw.circle(screen, BLACK, (snake_segments[-1][0]+5,snake_segments[-1][1]+15), 2)
-	elif direction == "right":
-		pygame.draw.circle(screen, BLACK, (snake_segments[-1][0]+15,snake_segments[-1][1]+15), 2)
-		pygame.draw.circle(screen, BLACK, (snake_segments[-1][0]+15,snake_segments[-1][1]+5), 2)
+#SNAKE CLASS
+class Snake:
+	def __init__(self):
+		#SNAKE PROPERTIES
+		self.x = 190
+		self.y = 160
+		self.x_change = 0
+		self.y_change = 0
+		self.speed = 6
+		self.length = 1
+		self.direction = ""
+		self.head = []
+		self.segments = []
+
+	def draw(self):
+		for segment in self.segments:
+			pygame.draw.rect(screen, BLUE, [segment[0], segment[1], 20, 20])
+		if self.direction == "up":
+			pygame.draw.circle(screen, BLACK, (self.segments[-1][0]+5,self.segments[-1][1]+5), 2)
+			pygame.draw.circle(screen, BLACK, (self.segments[-1][0]+15,self.segments[-1][1]+5), 2)
+		elif self.direction == "down":
+			pygame.draw.circle(screen, BLACK, (self.segments[-1][0]+5,self.segments[-1][1]+15), 2)
+			pygame.draw.circle(screen, BLACK, (self.segments[-1][0]+15,self.segments[-1][1]+15), 2)
+		elif self.direction == "left":
+			pygame.draw.circle(screen, BLACK, (self.segments[-1][0]+5,self.segments[-1][1]+5), 2)
+			pygame.draw.circle(screen, BLACK, (self.segments[-1][0]+5,self.segments[-1][1]+15), 2)
+		elif self.direction == "right":
+			pygame.draw.circle(screen, BLACK, (self.segments[-1][0]+15,self.segments[-1][1]+15), 2)
+			pygame.draw.circle(screen, BLACK, (self.segments[-1][0]+15,self.segments[-1][1]+5), 2)
 	
-		
-#SNAKE PROPERTIES
-x = 190
-y = 160
-x_change = 0
-y_change = 0
-speed = 6
-snake_segments = []
-length_snake = 1
-direction = ""
+	def eat_check(self, food):
+		if self.x == food.x - 10 and self.y == food.y - 10:
+			food.reposition()
+			self.length += 1
+
+	def move(self):
+		self.x += self.x_change
+		self.y += self.y_change
+		self.head = [self.x, self.y]
+		self.segments.append(self.head)
+		if len(self.segments) > self.length:
+			self.segments.pop(0)
+
+	def motion_direction(self, event):
+		if event.key == pygame.K_LEFT and self.direction != "right":
+			self.x_change = -30
+			self.y_change = 0
+			self.direction = "left"
+		elif event.key == pygame.K_RIGHT and self.direction !="left":
+			self.x_change = 30
+			self.y_change = 0
+			self.direction = "right"
+		elif event.key == pygame.K_UP and self.direction != "down":
+			self.x_change = 0
+			self.y_change = -30
+			self.direction = "up"			
+		elif event.key == pygame.K_DOWN and self.direction != "up":
+			self.x_change = 0
+			self.y_change = 30
+			self.direction = "down"
+
+#FOOD CLASS
+class Food:
+	def __init__(self):
+		#Properties of food
+		self.x = randrange(20,380, 30)
+		self.y = randrange(20, 350, 30)
+		self.radius = 10
+
+	def draw(self):
+		pygame.draw.circle(screen, RED, (self.x,self.y), self.radius)
+
+	def reposition(self):
+		self.x = randrange(20,380, 30)
+		self.y = randrange(20, 350, 30)
+
+
+#Class initiation
+snake = Snake()
+food = Food()
 
 #TICKRATE for snake speed
 clock = pygame.time.Clock()
-
-#Position of food
-food_x = random.randrange(20,380, 30)
-food_y = random.randrange(20, 350, 30)
 
 #Conditions for game to run
 game_running = True
@@ -87,56 +137,36 @@ while game_running:
 		if event.type == pygame.QUIT:
 			game_running = False
 		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT and direction != "right":
-				x_change = -30
-				y_change = 0
-				direction = "left"
-			elif event.key == pygame.K_RIGHT and direction !="left":
-				x_change = 30
-				y_change = 0
-				direction = "right"
-			elif event.key == pygame.K_UP and direction != "down":
-				x_change = 0
-				y_change = -30
-				direction = "up"			
-			elif event.key == pygame.K_DOWN and direction != "up":
-				x_change = 0
-				y_change = 30
-				direction = "down"
+			snake.motion_direction(event)
 
-	x += x_change
-	y += y_change
-
-	head = [x,y]
-	snake_segments.append(head)
-	if len(snake_segments) > length_snake:
-		snake_segments.pop(0)
+	snake.move()
 
 	screen.blit(background, (0,0))
-	textbox(font_base,f"Score = {length_snake-1}", BLACK, (120,355))
-	pygame.draw.circle(screen, RED, (food_x,food_y), 10)
-	if x < 10 or x > 390 or y < 10 or y > 310:
+	textbox(font_base,f"Score = {snake.length-1}", BLACK, (120,355))
+	food.draw()	
+
+	#DEATH CONDITION
+	if snake.x < 10 or snake.x > 390 or snake.y < 10 or snake.y > 310:
 		game_running = False
 		break
 	else:
-		snake(snake_segments)
+		snake.draw()
 
-	for segment in snake_segments[:-1]:
-		if segment == head:
+	for segment in snake.segments[:-1]:
+		if segment == snake.head:
 			game_running = False
 			break
+	
+	#EAT FOOD CONDITION
+	snake.eat_check(food)
 
-	if x == food_x - 10 and y == food_y - 10:
-		food_x = random.randrange(20,380, 30)
-		food_y = random.randrange(20, 350, 30)
-		length_snake += 1
 	pygame.display.update()
-	clock.tick(speed)
+	clock.tick(snake.speed)
 
 
 #Death Message Box
 textbox(font_base, "You have lost!", RED, (50,150))
-textbox(font_base, f"Your Score is {length_snake-1}", RED, (50,200))
+textbox(font_base, f"Your Score is {snake.length-1}", RED, (50,200))
 pygame.display.update()
 time.sleep(2)
 
